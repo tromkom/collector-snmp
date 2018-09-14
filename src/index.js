cff102f1f418af538734d5a8d0e33e3db0227f01
Kent Heide <kheide@users.noreply.github.com>
2018-09-14 15:57:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@
 const snmp = require('snmp-native');
 const config = require('config');
 const cluster = require('cluster');
+const collector = require('./lib/collector');
 
 const numOfCPU = require('os').cpus().length;
 
 if (cluster.isMaster) {
-	
+	for (let i = 0; i<numOfCPU; i++) {
+  		cluster.fork();
+  	}
+} else {
+	collector.start().then(() => {
+		console.log(`Collector ${process.pid} started..`); 
+	});
 }
