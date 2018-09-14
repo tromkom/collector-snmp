1541367b88aa23ce06cbe0b90ee4b38cfb35f663
Kent Heide <kh@kentheide.com>
2018-09-14 15:52:41 +0200

    primed entry point

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@
 const snmp = require('snmp-native');
 const config = require('config');
 const cluster = require('cluster');
+
+const numOfCPU = require('os').cpus().length;
+
+if (cluster.isMaster) {
+	
+}
